f89c134a652c1f66eb1eb3be0f7660f84cb5412a
Jeffrey Jesus <jefjesuswt@gmail.com>
2025-11-16 09:02:18 -0400

    +favicon.ico

--- src/app/pages/blog-page/blog-page.ts
+++ src/app/pages/blog-page/blog-page.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   imports: [],
   template: `
     <h2
-      class="block md:hidden text-3xl font-bold bg-linear-to-r from-teal-300 via-emerald-400 to-sky-400 bg-clip-text text-transparent anim-fade-in select-none leading-tight"
+      class="block text-3xl font-bold bg-linear-to-r from-teal-300 via-emerald-400 to-sky-400 bg-clip-text text-transparent anim-fade-in select-none leading-tight"
       aria-hidden="true"
     >
       [WIP]
